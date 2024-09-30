9ccffbec5282c2ae30b7471b0f820b41cbab0f00
Leire Garmendia <lgamendiadi20dw@ikzubirimanteo.com>
2024-09-30 13:05:08 +0200

    creando el dominio

--- src/main/java/com/proyeto/hand_craft_verse/dominio/Comprador.java
+++ src/main/java/com/proyeto/hand_craft_verse/dominio/Comprador.java
@@ -1,6 +1,9 @@
 package com.proyeto.hand_craft_verse.dominio;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,5 +12,6 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Comprador extends Usuario {
     
-    
+    @OneToMany(mappedBy = "comprador")
+    private List<Direccion> direccionesEnvio;
 }
