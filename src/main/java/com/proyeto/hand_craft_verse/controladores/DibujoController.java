ee46426b6c754d7dee9417ba1e45e4d7e977eaf8
leire <lgarmendiadi20dw@ikzubirimanteo.com>
2024-12-10 21:55:21 +0100

    intentando almacenar imagenes

--- src/main/java/com/proyeto/hand_craft_verse/controladores/DibujoController.java
+++ src/main/java/com/proyeto/hand_craft_verse/controladores/DibujoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 
 import java.io.InputStream;
